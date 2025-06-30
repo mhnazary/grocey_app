66e232ccb1b7636b4118dd5cd866f8236a0b85d0
MHNazary <mhnazary2020@gmail.com>
2025-06-30 23:43:13 +0430

    modify categories.dart

--- lib/data/categories.dart
+++ lib/data/categories.dart
@@ -12,10 +12,6 @@ const categories = {
   Categories.carbs: Category('Carbs', Color.fromARGB(255, 0, 60, 255)),
   Categories.sweets: Category('Sweets', Color.fromARGB(255, 255, 149, 0)),
   Categories.spices: Category('Spices', Color.fromARGB(255, 255, 187, 0)),
-  Categories.convenience: Category(
-    'Convenience',
-    Color.fromARGB(255, 191, 0, 255),
-  ),
   Categories.hygiene: Category('Hygiene', Color.fromARGB(255, 149, 0, 255)),
   Categories.other: Category('Other', Color.fromARGB(255, 0, 225, 255)),
 };
